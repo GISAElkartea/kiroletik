6083aca42da3d5e9c4f303ce8eb369b867d23f1e
Unai Zalakain <unai@gisa-elkartea.org>
2014-12-26 00:30:35 +0100

    Add a queryset of all sports to the base context

--- kiroletik/context_processors.py
+++ kiroletik/context_processors.py
@@ -1,10 +1,12 @@
 from banners.models import Banner
 from flatpages.models import Flatpage
 from widgets.models import Widget
+from sports.models import Sport
 
 
 def main_processor(request):
     return {
+        'sports': Sport.objects.all(),
         'banner': Banner.objects.active(),
         'flatpages': Flatpage.objects.published(),
         'widgets': Widget.objects.active(),
