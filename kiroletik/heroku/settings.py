633c82c9fb7d72a3211be1290ab437ae6b66c77e
Unai Zalakain <unai@gisa-elkartea.org>
2014-12-30 11:50:04 +0100

    Securing production settings (#14)

--- kiroletik/heroku/settings.py
+++ kiroletik/heroku/settings.py
@@ -6,20 +6,42 @@ from kiroletik.settings import * #noqa
 
 DEBUG = False
 
+DATABASES['default'] = dj_database_url.config()
+
+#####################
+# Security settings #
+#####################
+
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_string(
     50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'))
 
-DATABASES['default'] = dj_database_url.config()
+INSTALLED_APPS += ['djangosecure']
 
+MIDDLEWARE_CLASSES += ['djangosecure.middleware.SecurityMiddleware']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 60*60*24
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_FRAME_DENY = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
 
 ALLOWED_HOSTS = ['kiroletik.herokuapp.com',
                  'kiroletik.eus',
                  'www.kiroletik.eus']
 
+#################
+# Static assets #
+#################
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+###########
+# Logging #
+###########
 
 LOGGING = {
     "version": 1,
