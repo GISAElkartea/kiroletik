987f4812ecff035a90407ba78ccad03d30175a92
Unai Zalakain <unai@gisa-elkartea.org>
2014-12-30 22:47:07 +0100

    Fixed #21: Added Bugsnag for error handling

--- kiroletik/heroku/settings.py
+++ kiroletik/heroku/settings.py
@@ -102,6 +102,8 @@ EMAIL_SUBJECT_PREFIX = '[Kiroletik] '
 # Logging #
 ###########
 
+MIDDLEWARE_CLASSES += ['bugsnag.django.middleware.BugsnagMiddleware']
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
