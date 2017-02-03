6bec5cf2bb42f064f4e52d3d0d0937ae825827ed
Unai Zalakain <unai@gisa-elkartea.org>
2017-02-03 00:07:20 +0000

    Install django-acme

--- kiroletik/urls.py
+++ kiroletik/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^', include('sports.urls')),
     url(r'^flatpages/', include('flatpages.urls')),
 
@@ -13,7 +12,8 @@ urlpatterns = patterns(
     url(r'^autocomplete/', include('autocomplete_light.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
-)
+    url(r'^.well-known/acme-challenge/', include('django_acme.urls')),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
